3cfbc6a71d6aeed1926f2a219159593d97e98a77
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-23 03:38:42 +0100

    Add InvitePage component for redirection

---
+++ app/invite/[code]/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation";
+
+export default async function InvitePage({
+  params,
+}: {
+  params: Promise<{ code: string }>;
+}) {
+  const { code } = await params;
+
+  redirect(`/?ref=${encodeURIComponent(code)}`);
+}
