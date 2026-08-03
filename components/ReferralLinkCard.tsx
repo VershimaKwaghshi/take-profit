d7d78621067b12f6a480f869ba3ce414480c6194
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-08-03 06:45:15 +0100

    Update ReferralLinkCard.tsx

--- components/ReferralLinkCard.tsx
+++ components/ReferralLinkCard.tsx
@@ -52,7 +52,6 @@ export default function ReferralLinkCard() {
           {copied ? "Copied" : "Copy Link"}
         </button>
 
-
         <a
           href={referralLink ? `https://wa.me/?text=${encodeURIComponent(referralLink)}` : "#"}
           target="_blank"
