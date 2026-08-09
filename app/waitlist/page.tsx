b25df77474703121b72d7e1323ce3b1157a94b68
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-08-09 10:38:02 +0100

    Add files via upload

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,1 +1,19 @@
+import Navbar from "@/components/Navbar";
+import Waitlist from "@/components/Waitlist";
+import Footer from "@/components/Footer";
 
+export default function WaitlistPage() {
+  return (
+    <main className="min-h-screen bg-white">
+
+      <Navbar />
+
+      <div className="pt-32">
+        <Waitlist />
+      </div>
+
+      <Footer />
+
+    </main>
+  );
+}
