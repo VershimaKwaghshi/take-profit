dfdc6b8023552169ad5d685a50bff9f1068f1f4a
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-08-18 08:55:04 +0100

    Update page.tsx

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,19 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Waitlist from "@/components/Waitlist";
-import Footer from "@/components/Footer";
+import { redirect } from "next/navigation";
 
 export default function WaitlistPage() {
-  return (
-    <main className="min-h-screen bg-white">
-
-      <Navbar />
-
-      <div className="pt-32">
-        <Waitlist />
-      </div>
-
-      <Footer />
-
-    </main>
-  );
+  redirect("/register");
 }
